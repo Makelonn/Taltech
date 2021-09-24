716c7a643144bced74dacef24403739f03606ab7
Makelonn <maelie.lebaron@gmail.com>
2021-09-24 13:58:15 +0300

    haskell : task 2 is done ggwp

--- haskell/course2/task2_4.hs
+++ haskell/course2/task2_4.hs
@@ -1,24 +1,31 @@
 -- Removes consecutive duplicate from the list
 -- init <- remove the last elem
 removeDup :: [Int] -> [Int]
+removeDup [] = []
 removeDup [x] = [x]
-removeDup [x, y] = if x == y then [x] else [x,y]
 removeDup entryList =
     if x == y then
         x : removeDup the_tail
-    else [x,y] ++  removeDup the_tail
+    else x: removeDup (y:the_tail)
     where
         x = head entryList
-        y = head entryList
-        the_tail = if null entryList then [] else tail (tail entryList)
+        y =entryList!!1 
+        the_tail = tail (tail entryList)
 
 main=do
     let aList1 = [1,2,3,3,4,5,6]
-    let aList2 = [1,1,2,2,3,3,4,4,5,5,6,6]
+    let aList2 = [1,2,3,4,5,6,6]
     let aList3 = [1,1,2,3,4,5,6]
-    let aList4 = [1,2,3,4,5,6,6]
+    let aList4 = [1,1,2,2,3,3,4,4,5,5,6,6]
+    --let aList5 = [1,2,1,3,4,5,6,6]
 
+    putStr(show aList1 ++ " gives : ")
     print(removeDup aList1)
+    putStr(show aList2 ++ " gives : ")
     print(removeDup aList2)
+    putStr(show aList3 ++ " gives : ")
     print(removeDup aList3)
+    putStr(show aList4 ++ " gives : ")
     print(removeDup aList4)
+    --putStr(show aList5 ++ "gives : ")
+    --print(removeDup aList5)
