53993760f049e289680aa12b0b31d89c5d324901
Makelonn <maelie.lebaron@gmail.com>
2021-09-24 04:15:54 +0300

    haskell : task 2 alsmost done

--- haskell/course2/task2_3.hs
+++ haskell/course2/task2_3.hs
@@ -1,17 +1,18 @@
+import Control.Monad.Trans.State (put)
 -- Fibonnacci sequence up to an index
 
 fibonnacciToIndex :: Int -> [Int]
-fibonnacciToIndex 1 = [0,1]
+fibonnacciToIndex 1 = [1,0]
 fibonnacciToIndex index =
-    [sum, minus1, minus2] ++ minus1List
+    mysum : minus1List
     where
         minus1List = fibonnacciToIndex (index-1)
         minus1:minus2:xs = minus1List
-        sum = minus1 + minus2
+        mysum = minus1 + minus2
 
 main=do
     putStrLn "Enter a number : "
     num <- getLine
     let index_fib = (read num :: Int)
-    putStrLn("The Fibonnacci sequence up to the " ++ show index_fib ++ " is ")
-    let fibo_list = fibonnacciToIndex index_fib
+    putStr("The Fibonnacci sequence up to the " ++ show index_fib ++ " is ")
+    print(fibonnacciToIndex index_fib)
--- haskell/course2/task2_4.hs
+++ haskell/course2/task2_4.hs
@@ -1,21 +1,22 @@
 -- Removes consecutive duplicate from the list
 -- init <- remove the last elem
-removeDup :: [a] -> [a]
+removeDup :: [Int] -> [Int]
 removeDup [x] = [x]
-removeDup entryList = 
-    if (x == y) then 
-        outList <- removeDup x ++ [tail entryList]
-    else outList <- [x,y] ++  removeDup tail entryList
+removeDup entryList =
+    if x == y then
+        x : removeDup the_tail
+    else [x,y] ++  removeDup the_tail
     where
         x:y:xs = entryList
+        the_tail = if null entryList then [] else tail (tail entryList)
 
 main=do
-    aList1 = [1,2,3,3,4,5,6]
-    aList2 = [1,1,2,2,3,3,4,4,5,5,6,6]
-    aList3 = [1,1,2,3,4,5,6]
-    aList4 = [1,2,3,4,5,6,6]
+    let aList1 = [1,2,3,3,4,5,6]
+    let aList2 = [1,1,2,2,3,3,4,4,5,5,6,6]
+    let aList3 = [1,1,2,3,4,5,6]
+    let aList4 = [1,2,3,4,5,6,6]
 
-    putStrLn(removeDup aList1)
-    putStrLn(removeDup aList2)
-    putStrLn(removeDup aList3)
-    putStrLn(removeDup aList4)
+    print(removeDup aList1)
+    print(removeDup aList2)
+    print(removeDup aList3)
+    print(removeDup aList4)
